fd5dea43ad9ecbfbecaa9f037b877d07e1bc9830
Max Wu <maxwu@Maxs-MacBook-Pro.local>
2017-08-20 00:05:11 -0400

    1.1 complete

---
+++ Arrays&Strings.cc
@@ -0,0 +1,77 @@
+
+
+//Solution with a data structure
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+//Change to the question you want to run
+#define ONE_POINT_ONE
+
+
+
+
+#ifdef ONE_POINT_ONE
+
+/*
+1.1 IS UNIQUE: Implement an algorithm to determine if a string has all unique
+               characters. What if you cannot use additional data structures?
+
+Assumptions: String is in ASCII which contains 128 characters
+*/
+
+//Solution with a datastructure, O(n) runtime, O(1) space complexity
+bool unique_char(string s) {
+  //First check if string contains more than 128 characters
+  if (s.length() > 128) return false;
+  //Create a binary array that tracks all characters, initialized to false
+  bool ascii_lst[128] = {0};
+  //Iterate, iterate, iterate ...
+  for (auto c : s) {
+    int i = c;
+    if (ascii_lst[i] == false) {
+      ascii_lst[i] = true;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+//Solution without a data structure, assuming s can be modified otherwise check
+//each char against all char after it in the string.
+bool unique_char_wds(string s) {
+  //Sort the string
+  sort(s.begin(), s.end());
+
+  //Iterate through the array to check if there are same chars side by side
+
+  for (int i = 1; i < s.length(); ++i) {
+    if (s.at(i-1) == s.at(i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+#endif
+
+
+
+
+
+int main()
+{
+    #ifdef ONE_POINT_ONE
+    string s = "aab";
+    bool t = unique_char(s);
+    if (t) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    #endif
+}
